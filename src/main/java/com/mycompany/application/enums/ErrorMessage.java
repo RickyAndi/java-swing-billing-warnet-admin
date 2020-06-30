03dc37050ffe4a2f47a5b0462c8028180d225e3f
RickyAndhi <rickypandhika@gmail.com>
2020-06-30 16:30:19 +0700

    Add error message

--- src/main/java/com/mycompany/application/enums/ErrorMessage.java
+++ src/main/java/com/mycompany/application/enums/ErrorMessage.java
@@ -2,6 +2,7 @@ package com.mycompany.application.enums;
 
 public enum ErrorMessage {
 
+    GENERAL_UNKNOWN_ERROR("Mohon maaf, telah terjadi error"),
     ADMIN_DOES_NOT_EXIST("Akun admin tidak ditemukan, mohon masukan username dan password yang benar");
 
     public final String message;
