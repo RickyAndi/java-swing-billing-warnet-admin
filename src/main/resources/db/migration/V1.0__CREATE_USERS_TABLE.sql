3870f282b195b91b3927e31b50c2d834540e3282
RickyAndhi <rickypandhika@gmail.com>
2020-06-30 16:28:25 +0700

    Fix error sql

--- src/main/resources/db/migration/V1.0__CREATE_USERS_TABLE.sql
+++ src/main/resources/db/migration/V1.0__CREATE_USERS_TABLE.sql
@@ -2,11 +2,10 @@
 drop table if exists users;
 
 create table users (
-    id int(11) auto_increment,
+    id int(11) auto_increment primary key,
     username varchar(255) not null,
-    password varchar(255) not null
-    primary key (id)
-) engine = innodb;
+    `password` varchar(255) not null
+) ENGINE = INNODB;
 
 insert into users (username, password) values ("admin", "5f4dcc3b5aa765d61d8327deb882cf99");
 insert into users (username, password) values ("root", "5f4dcc3b5aa765d61d8327deb882cf99");
