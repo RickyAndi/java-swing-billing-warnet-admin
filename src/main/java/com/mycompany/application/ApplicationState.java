0eaec58fd61a2b9f2c086e116ac5f8b2052944e2
RickyAndhi <rickypandhika@gmail.com>
2020-06-30 15:35:18 +0700

    Add application state object

--- src/main/java/com/mycompany/application/ApplicationState.java
+++ src/main/java/com/mycompany/application/ApplicationState.java
@@ -5,21 +5,48 @@
  */
 package com.mycompany.application;
 
-import com.mycompany.gui.MainWindow;
+import com.mycompany.application.entities.User;
+import com.mycompany.application.enums.ErrorMessage;
+import com.mycompany.application.exceptions.UserDoesNotExistException;
+import com.mycompany.application.repositories.UserRepository;
+
+import java.util.Optional;
 
 /**
  *
  * @author rickyandhi
  */
-public class Application {
-    public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                MainWindow mainWindow = new MainWindow();
-                mainWindow.dispose();
-                mainWindow.setUndecorated(true);
-                mainWindow.setVisible(true);
-            }
-        });
+public class ApplicationState {
+
+    private Optional<User> currentUser;
+
+    private UserRepository userRepository;
+
+    public ApplicationState(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(Optional<User> currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public void login(String username, String plainPassword) throws UserDoesNotExistException {
+        Optional<User> userOptional = this.userRepository
+                .getUserByUsernameAndPassword(username, plainPassword);
+
+        setCurrentUser(userOptional);
+
+        if (!isUserBeingLoggedIn()) {
+            throw new UserDoesNotExistException(ErrorMessage.ADMIN_DOES_NOT_EXIST.message);
+        }
+    }
+
+    public Boolean isUserBeingLoggedIn() {
+        return this.currentUser.isPresent();
     }
 }
