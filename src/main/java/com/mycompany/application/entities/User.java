34f1045055116a99abc090c96b56fe1ade6895a0
RickyAndhi <rickypandhika@gmail.com>
2020-06-30 16:28:44 +0700

    Fix user entity

--- src/main/java/com/mycompany/application/entities/User.java
+++ src/main/java/com/mycompany/application/entities/User.java
@@ -5,10 +5,45 @@
  */
 package com.mycompany.application.entities;
 
+import javax.persistence.*;
+
 /**
  *
  * @author rickyandhi
  */
+
+@Entity
+@Table(name="users", uniqueConstraints={@UniqueConstraint(columnNames={"id"})})
 public class User {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true, length = 11)
+    private int id;
+
+    @Column(name="username", length = 255, nullable = false)
+    private String username;
+
+    @Column(name="password", length = 255, nullable = false)
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
