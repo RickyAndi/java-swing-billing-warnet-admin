c887e8752de5c2e6b1cbb9175f84327131cc6f0a
RickyAndhi <rickypandhika@gmail.com>
2020-07-05 19:36:54 +0700

    Change method name

--- src/main/java/com/mycompany/application/enums/ErrorMessageEnum.java
+++ src/main/java/com/mycompany/application/enums/ErrorMessageEnum.java
@@ -1,13 +1,13 @@
 package com.mycompany.application.enums;
 
-public enum ErrorMessage {
+public enum ErrorMessageEnum {
 
     GENERAL_UNKNOWN_ERROR("Mohon maaf, telah terjadi error"),
     ADMIN_DOES_NOT_EXIST("Akun admin tidak ditemukan, mohon masukan username dan password yang benar");
 
     public final String message;
 
-    private ErrorMessage(String message) {
+    private ErrorMessageEnum(String message) {
         this.message = message;
     }
 }
